2f377c622c54e735ae9c2ee9f4a7bca344015a8d
Sebastian Sandoval <sebassandoval@hotmail.com>
2022-07-18 21:12:04 -0500

    Basic structure added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="test">Solicita tu turno virtual</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/colors.js
@@ -0,0 +1,11 @@
+export default {
+  grayTitle: "#2D2D2D",
+  grayText: "#3F3F41",
+  gray: "#00000029",
+  graylight: "#00000029",
+  grayMedium: "#979797",
+  grayDark: "#575757",
+  black: "#191919",
+  green: "#1F9547",
+  white: "#FFFFFF",
+};
