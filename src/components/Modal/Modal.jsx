d1a5e7da9d0f6198a30b3f4b38139f83a5f0dba6
Sebastian Sandoval <sebassandoval@hotmail.com>
2022-07-20 16:07:33 -0500

    PDF function added

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, ModalContainer } from "./modal.styled";
 import Logo from "../../assets/images/logo.png";
-import ButtonSend from "../ButtonSend/ButtonSend";
+import ReactToPdf from "react-to-pdf";
 
 const Modal = ({ data, close }) => {
   const {
@@ -12,22 +12,38 @@ const Modal = ({ data, close }) => {
     lastName,
     secondLastName,
   } = data;
+
   return (
-    <ModalContainer>
-      <div className="container">
-        <div className="texts">
-          <img src={Logo} alt="Logo" />
-          <h2 className="title">¡Has solicitado tu turno con éxito!</h2>
-          <h3 className="type">Servicio:</h3>
-          <h3 className="text">Consulta Externa</h3>
-          <h3 className="type">Día:</h3>
-          <h3 className="text">10-05-2021</h3>
-          <h3 className="type">Hora:</h3>
-          <h3 className="text">02:00 p.m</h3>
-          <Button onClick={close}>Solicitar nuevo turno</Button>
-        </div>
-      </div>
-    </ModalContainer>
+    <ReactToPdf
+      filename="turno.pdf"
+      onComplete={() => {
+        close();
+      }}
+    >
+      {({ toPdf, targetRef }) => (
+        <ModalContainer>
+          <div className="container" ref={targetRef}>
+            <div className="texts">
+              <img src={Logo} alt="Logo" />
+              <h2 className="title">¡Has solicitado tu turno con éxito!</h2>
+              <h3 className="type">Servicio:</h3>
+              <h3 className="text">Consulta Externa</h3>
+              <h3 className="type">Día:</h3>
+              <h3 className="text">10-05-2021</h3>
+              <h3 className="type">Hora:</h3>
+              <h3 className="text">02:00 p.m</h3>
+              <Button
+                onClick={() => {
+                  toPdf();
+                }}
+              >
+                Solicitar nuevo turno
+              </Button>
+            </div>
+          </div>
+        </ModalContainer>
+      )}
+    </ReactToPdf>
   );
 };
 
